d878db909c6f5f06b4fb888e14e4f26afc116f34
Deniz Tutucu <denizsafaktutuncu@gmail.com>
2019-04-15 13:42:06 -0600

    Adds TabelViewController and the cell

---
+++ MeetTheDreamTeam/PersonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonTableViewCell.swift
+//  MeetTheDreamTeam
+//
+//  Created by Deniz Tutuncu on 4/15/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    
+
+}
---
+++ Model Controller/PersonController.swift
@@ -0,0 +1,13 @@
+//
+//  PersonController.swift
+//  MeetTheDreamTeam
+//
+//  Created by Deniz Tutuncu on 4/15/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+import Foundation
+
+class PersonController {
+    
+}
